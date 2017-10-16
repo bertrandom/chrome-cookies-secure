a92e71bbb9eda2ab8ded388727105ffc3e1590de
Bertrand Fan <bertrand@fan.net>
2017-10-15 20:34:47 -0700

    Update examples

---
+++ example/double-google.js
@@ -0,0 +1,14 @@
+var chrome = require('../index');
+
+chrome.getCookies('http://google.com', function (err, cookies) {
+	chrome.getCookies('http://google.com', function (err, cookies) {
+
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		console.log(cookies);
+
+	});
+});
