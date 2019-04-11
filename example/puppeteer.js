9e7976891e4607c7e9cd670c0cb17b6cb78630af
Ruben G Murray <rubengmurray@gmail.com>
2019-04-11 10:51:45 +0100

    had not added new puppeteer.js file to repo

---
+++ example/puppeteer.js
@@ -0,0 +1,30 @@
+// const browser = await puppeteer.launch({ 
+//  // your args
+// });
+
+// const page = await browser.newPage();
+
+let cookiesToSave;
+
+chrome.getCookies('https://yourURL.com', 'puppeteer', function(err, cookies) {
+    if (err) {
+        console.log({message: 'error', err});
+        return
+    }
+    cookiesToSave = cookies
+}, 'YourChromeProfile')
+
+// await page.waitFor(2000);
+await page.setCookie(...cookiesToSave);
+
+// let result = await page.evaluate(() => {
+//     let stuff;
+//     // do stuff here
+//     return stuff; 
+// })
+// .catch((e) => {
+//     // catch any errors
+// });
+
+// browser.close();
+// return result
