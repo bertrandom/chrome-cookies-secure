716508cba5eec0fec5a73b58854430d4f2ef61b0
friday <friday.thx@gmail.com>
2020-08-14 16:59:21 +0800

    Add a typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+declare module "chrome-cookies-secure" {
+  type PuppeteerCookie = {
+    name: string;
+    value: string;
+    expires: number;
+    domain: string;
+    path: string;
+    HttpOnly?: boolean;
+  };
+
+  type Callback<T> = (err: Error, cookies: T) => void;
+
+  function getCookies(url: string, cb: Callback<Record<string, string>>): void;
+
+  function getCookies(
+    url: string,
+    format: "object",
+    cb: Callback<Record<string, string>>
+  ): void;
+
+  function getCookies(
+    url: string,
+    format: "curl" | "header",
+    cb: Callback<string>
+  ): void;
+
+  function getCookies(url: string, format: "jar", cb: Callback<any>): void;
+
+  function getCookies(
+    url: string,
+    format: "set-cookie",
+    cb: Callback<string[]>
+  ): void;
+
+  function getCookies(
+    url: string,
+    format: "puppeteer",
+    cb: Callback<PuppeteerCookie[]>
+  ): void;
+}
