48b6069482515c5b3e795fec21f0dd26e2855f84
= <=>
2022-06-03 21:52:28 +0100

    skip tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -19,7 +19,7 @@ const url = 'https://www.google.com'
 
 // These tests are not part of a CI / CD
 // To run locally, they assumes you have some cookies for google.com under the 'Default' browser profile
-it('Should get basic cookies from the defined url', async () => {
+xit('Should get basic cookies from the defined url', async () => {
     const cookiesBasic = await chrome.getCookiesPromised(url)
     if (!Object.keys(cookiesBasic).length) {
         throw new Error('No cookie found')
@@ -28,7 +28,7 @@ it('Should get basic cookies from the defined url', async () => {
     // await joi.validate(cookiesBasic, joi.object().required());
 }).timeout(3000)
 
-it('Should get puppeteer cookies from the defined url', async () => {
+xit('Should get puppeteer cookies from the defined url', async () => {
     const cookiesPuppeteer = await chrome.getCookiesPromised(url, 'puppeteer')
     await joi.validate(cookiesPuppeteer, puppeteerCookie);
 }).timeout(3000)
