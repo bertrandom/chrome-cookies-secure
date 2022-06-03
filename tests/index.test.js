7252a0f5da1c5a36e9dd88f6c6fa2c371ce3a305
= <=>
2022-06-03 21:10:47 +0100

    move joi to dev dependency

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,15 +1,19 @@
 const chrome = require('../index')
 const joi = require('joi')
 
-const puppeteerCookie = joi.array().items({
-    name: joi.string().required(),
-    value: joi.string().required(),
-    expires: joi.any().required(), // Should be a number but not necessarily a javascript safe one, which causes joi the fail
-    domain: joi.string().required(),
-    path: joi.string().required(),
-    Secure: joi.boolean().optional(),
-    HttpOnly: joi.boolean().optional(),
-}).required().min(1)
+const puppeteerCookie = joi
+    .array()
+    .items({
+        name: joi.string().required(),
+        value: joi.string().required(),
+        expires: joi.any().required(), // Should be a number but not necessarily a javascript safe one, which causes joi the fail
+        domain: joi.string().required(),
+        path: joi.string().required(),
+        Secure: joi.boolean().optional(),
+        HttpOnly: joi.boolean().optional(),
+    })
+    .required()
+    .min(1)
 
 const url = 'https://www.google.com'
 
